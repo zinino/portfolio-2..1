59159ce8b1fd359a5cadf53d56fdfee03df0a5f7
zi nino <bbereket115@gmail.com>
2023-07-05 11:13:41 +0200

    commit

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const navToggle = document.querySelector(".nav-toggle");
 const Nav = document.querySelector(".nav");
 const navLink = document.querySelectorAll(".nav-link");
+const liveBlack = document.getElementById("live");
 
+liveBlack.addEventListener("click", function () {
+  alert(
+    "site deploying in progress sorry for the inconvienence it will be back soon :)"
+  );
+});
 navToggle.addEventListener("click", function () {
   document.body.classList.toggle("nav-open");
 });
